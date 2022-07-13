0fd63c6cd8752df7953a2b96bae72809d6594977
Luan Vale <luancabral5566@gmail.com>
2022-07-13 17:55:55 -0300

    fix: troquei a imagem local por web.

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
+
+      <Text style={estilosTextos.textoTitulo}>Hello World!</Text>
+      
+      <Image 
+      source={ { uri: 'https://cdn-icons-png.flaticon.com/512/25/25231.png' } }
+      style={estilosImagem.imgLogo}
+      /> 
+
+      <View style={styles.quadrado}>
+        <Text style={estilosTextos.texto}>Esse é meu primeiro aplicativo.</Text>
+        <Text style={estilosTextos.texto}>Criado por Luan Cabral.</Text>
+      </View>
+
     </View>
   );
 }
@@ -16,5 +29,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    },
+  quadrado: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 100,
+    borderRadius: 8,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    },
+});
+
+const estilosTextos = StyleSheet.create({
+  textoTitulo: {
+    margin: 10,
+    padding: 20,
+    fontSize: 36,
+    borderWidth: 2,
+    borderRadius: 3,
+    backgroundColor: '#fff',
+  },
+  texto: {
+    margin: 10,
+    fontSize: 18,
+  },
+});
+
+const estilosImagem = StyleSheet.create({
+  imgLogo: {
+    margin: 20,
+    width: 100,
+    height: 100,
   },
 });
