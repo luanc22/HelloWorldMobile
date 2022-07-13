c281347925906018a10e88807509754a1004f5f9
Luan Vale <luancabral5566@gmail.com>
2022-07-13 18:18:50 -0300

    refact: alteração do titulo e textos.

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ export default function App() {
   return (
     <View style={styles.container}>
 
+      <View style={styles.barraTitulo}>
+      <Text style={estilosTextos.textoTitulo}>Hello World!</Text>
+      </View>
+      
       <StatusBar style="auto" />
 
-      <Text style={estilosTextos.textoTitulo}>Hello World!</Text>
-      
       <Image 
       source={ { uri: 'https://cdn-icons-png.flaticon.com/512/25/25231.png' } }
       style={estilosImagem.imgLogo}
@@ -16,7 +18,7 @@ export default function App() {
 
       <View style={styles.quadrado}>
         <Text style={estilosTextos.texto}>Esse é meu primeiro aplicativo.</Text>
-        <Text style={estilosTextos.texto}>Criado por Luan Cabral.</Text>
+        <Text style={estilosTextos.texto}>Criado por <Text style={ estilosTextos.destaque }>Luan Cabral</Text>.</Text>
       </View>
 
     </View>
@@ -40,6 +42,12 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderWidth: 2,
     },
+    barraTitulo: {
+      backgroundColor: '#fff',
+      height: 100,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
 });
 
 const estilosTextos = StyleSheet.create({
@@ -48,13 +56,17 @@ const estilosTextos = StyleSheet.create({
     padding: 20,
     fontSize: 36,
     borderWidth: 2,
-    borderRadius: 3,
+    borderRadius: 8,
     backgroundColor: '#fff',
   },
   texto: {
     margin: 10,
     fontSize: 18,
   },
+  destaque: {
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+  }
 });
 
 const estilosImagem = StyleSheet.create({
